4287831363f83c7698d564a605151b5ce31fb4bf
Daniel Bettles <danbettles@yahoo.co.uk>
2022-03-08 21:58:46 +0000

    Added 'Gitignore' for working with gitignore files.

--- src/Gitignore.php
+++ src/Gitignore.php
@@ -4,6 +4,116 @@ declare(strict_types=1);
 
 namespace PowderBlue\GitUtils;
 
+use InvalidArgumentException;
+use RuntimeException;
+
+use function array_splice;
+use function count;
+use function file;
+use function file_get_contents;
+use function file_put_contents;
+use function implode;
+use function is_file;
+use function preg_match;
+use function rtrim;
+
+use const false;
+use const FILE_APPEND;
+use const PHP_EOL;
+
 class Gitignore
 {
+    private string $filename;
+
+    public function __construct(string $filename)
+    {
+        $this->setFilename($filename);
+    }
+
+    private function getLines(): array
+    {
+        return file($this->getFilename()) ?: [];
+    }
+
+    /** @return int|false */
+    public function findPattern(string $pattern)
+    {
+        foreach ($this->getLines() as $i => $line) {
+            if ($pattern === rtrim($line)) {
+                return $i;
+            }
+        }
+
+        return false;
+    }
+
+    public function containsPattern(string $pattern): bool
+    {
+        return false !== $this->findPattern($pattern);
+    }
+
+    /**
+     * Appends the patterns, starting on a new line.
+     *
+     * @param string[] $patterns
+     */
+    public function appendPatterns(array $patterns): self
+    {
+        $fileContent = file_get_contents($this->getFilename());
+
+        // If the file doesn't end with a newline then we'll need to add one -- so that all the patterns occupy their
+        // own, separate line.
+        $prefix = !$fileContent || preg_match('~[\r\n]$~', $fileContent)
+            ? ''
+            : PHP_EOL
+        ;
+
+        $bytesWritten = file_put_contents(
+            $this->getFilename(),
+            $prefix . implode(PHP_EOL, $patterns),
+            FILE_APPEND
+        );
+
+        if (!$bytesWritten) {
+            throw new RuntimeException("Failed to append patterns to `{$this->getFilename()}`.");
+        }
+
+        return $this;
+    }
+
+    public function insertPatternAtLineNo(string $pattern, int $lineNo): self
+    {
+        $lines = $this->getLines();
+
+        // We'll need to append an EOL if we're inserting between lines (before the last line).
+        $eol = $lineNo < count($lines)
+            ? PHP_EOL
+            : ''
+        ;
+
+        array_splice($lines, $lineNo, 0, ["{$pattern}{$eol}"]);
+        $bytesWritten = file_put_contents($this->getFilename(), $lines);
+
+        if (!$bytesWritten) {
+            throw new RuntimeException("Failed to insert pattern into `{$this->getFilename()}`.");
+        }
+
+        return $this;
+    }
+
+    private function setFilename(string $filename): self
+    {
+        if (!is_file($filename)) {
+            throw new InvalidArgumentException("The `.gitignore` (`{$filename}`) does not exist.");
+        }
+
+        $this->filename = $filename;
+
+        return $this;
+    }
+
+    public function getFilename(): string
+    {
+        return $this->filename;
+    }
 }
--- tests/src/GitignoreTest.php
+++ tests/src/GitignoreTest.php
@@ -4,8 +4,256 @@ declare(strict_types=1);
 
 namespace PowderBlue\GitUtils\Tests;
 
+use InvalidArgumentException;
 use PHPUnit\Framework\TestCase;
+use PowderBlue\GitUtils\Gitignore;
+use RuntimeException;
+
+use function copy;
+use function file_get_contents;
+use function implode;
+use function sys_get_temp_dir;
+use function tempnam;
+use function unlink;
+
+use const false;
+use const PHP_EOL;
+use const true;
 
 class GitignoreTest extends TestCase
 {
+    public function testConstructor()
+    {
+        $filename = $this->createFixtureFilename('empty');
+
+        $gitignore = new Gitignore($filename);
+
+        $this->assertSame($filename, $gitignore->getFilename());
+    }
+
+    public function testConstructorThrowsAnExceptionIfTheFileDoesNotExist()
+    {
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage('does not exist');
+
+        $filename = $this->createFixtureFilename('Supercalifragilisticexpialidocious');
+
+        new Gitignore($filename);
+    }
+
+    public function providesForTestfindpattern(): array
+    {
+        return [
+            [
+                false,  // Line no.
+                false,  // Contains pattern?
+                $this->createFixtureFilename('empty'),
+                'anything',
+            ],
+            [
+                false,
+                false,
+                $this->createFixtureFilename('files_and_dirs'),
+                'baz',
+            ],
+            [
+                false,
+                false,
+                $this->createFixtureFilename('files_and_dirs'),
+                '/baz',
+            ],
+            [
+                false,
+                false,
+                $this->createFixtureFilename('files_and_dirs'),
+                '/baz/',
+            ],
+            [
+                2,
+                true,
+                $this->createFixtureFilename('files_and_dirs'),
+                'baz/',
+            ],
+        ];
+    }
+
+    /**
+     * @dataProvider providesForTestfindpattern
+     */
+    public function testFindpattern(
+        $lineNumOfPattern,
+        $fileContainsPattern,
+        $filename,
+        $pattern
+    ) {
+        $gitignore = new Gitignore($filename);
+
+        $this->assertSame($lineNumOfPattern, $gitignore->findPattern($pattern));
+    }
+
+    /**
+     * @dataProvider providesForTestfindpattern
+     */
+    public function testContainspattern(
+        $lineNumOfPattern,
+        $fileContainsPattern,
+        $filename,
+        $pattern
+    ) {
+        $gitignore = new Gitignore($filename);
+
+        $this->assertSame($fileContainsPattern, $gitignore->containsPattern($pattern));
+    }
+
+    public function providesForTestappendpatterns(): array
+    {
+        return [
+            [
+                implode(PHP_EOL, [
+                    'qux',
+                    'quux',
+                ]),
+                $this->createFixtureFilename('empty'),
+                [
+                    'qux',
+                    'quux',
+                ],
+            ],
+            [
+                implode(PHP_EOL, [
+                    'foo',
+                    'bar',
+                    'baz',
+                    'qux',
+                    'quux',
+                ]),
+                $this->createFixtureFilename('with_trailing_newline'),
+                [
+                    'qux',
+                    'quux',
+                ],
+            ],
+            [
+                implode(PHP_EOL, [
+                    'foo',
+                    'bar',
+                    'baz',
+                    'qux',
+                    'quux',
+                ]),
+                $this->createFixtureFilename('without_trailing_newline'),
+                [
+                    'qux',
+                    'quux',
+                ],
+            ],
+        ];
+    }
+
+    /**
+     * @dataProvider providesForTestappendpatterns
+     */
+    public function testAppendpatterns(
+        $expectedFileContent,
+        $templateFilename,
+        $patterns
+    ) {
+        $tempFilePath = $this->createTempFileFromTemplate($templateFilename);
+
+        $gitignore = new Gitignore($tempFilePath);
+        $something = $gitignore->appendPatterns($patterns);
+
+        $tempFileContent = file_get_contents($tempFilePath);
+        unlink($tempFilePath);
+
+        $this->assertSame($gitignore, $something);
+        $this->assertSame($expectedFileContent, $tempFileContent);
+    }
+
+    public function providesForTestinsertpatternatlineno(): array
+    {
+        return [
+            [
+                implode(PHP_EOL, [
+                    'foo',
+                ]),
+                $this->createFixtureFilename('empty'),
+                'foo',
+                0,
+            ],
+            [
+                implode(PHP_EOL, [
+                    'qux',
+                    'foo',
+                    'bar',
+                    'baz',
+                    '',
+                ]),
+                $this->createFixtureFilename('with_trailing_newline'),
+                'qux',
+                0,
+            ],
+            [
+                implode(PHP_EOL, [
+                    'foo',
+                    'qux',
+                    'bar',
+                    'baz',
+                    '',
+                ]),
+                $this->createFixtureFilename('with_trailing_newline'),
+                'qux',
+                1,
+            ],
+            [
+                implode(PHP_EOL, [
+                    'foo',
+                    'bar',
+                    'baz',
+                    'qux',
+                ]),
+                $this->createFixtureFilename('with_trailing_newline'),
+                'qux',
+                3,
+            ],
+        ];
+    }
+
+    /**
+     * @dataProvider providesForTestinsertpatternatlineno
+     */
+    public function testInsertpatternatlineno(
+        $expectedFileContent,
+        $templateFilename,
+        $pattern,
+        $lineNo
+    ) {
+        $tempFilePath = $this->createTempFileFromTemplate($templateFilename);
+
+        $gitignore = new Gitignore($tempFilePath);
+        $something = $gitignore->insertPatternAtLineNo($pattern, $lineNo);
+
+        $tempFileContent = file_get_contents($tempFilePath);
+        unlink($tempFilePath);
+
+        $this->assertSame($gitignore, $something);
+        $this->assertSame($expectedFileContent, $tempFileContent);
+    }
+
+    private function createFixtureFilename(string $basename): string
+    {
+        return __DIR__ . "/GitignoreTest/{$basename}";
+    }
+
+    private function createTempFileFromTemplate(string $templateFilename): string
+    {
+        $tempFilePath = tempnam(sys_get_temp_dir(), '');
+        $copied = copy($templateFilename, $tempFilePath);
+
+        if (!$copied) {
+            throw new RuntimeException("Failed to copy template file `{$templateFilename}`.");
+        }
+
+        return $tempFilePath;
+    }
 }
